34e4aa953e3b538ba08854ab2e45f61edff46402
IJuanTM <iwan2510@gmail.com>
2026-05-06 23:03:36 +0200

    Update confirmation prompt format in install.js; bump version to 2.8.3

--- install.js
+++ install.js
@@ -155,7 +155,7 @@ const listAddons = (addons) => addons.forEach((name, i) => out(PAD + C.cyan + `$
 const confirmSuggestion = async (suggestion) => {
   line();
   while (true) {
-    const a = (await promptUser(PAD + `${C.cyan}◌${C.reset} ${C.dim}Did you mean${C.reset} ${C.cyan}${suggestion}${C.reset}${C.dim}? [y/${C.reset}N${C.dim}]${C.reset}`, 'no')).toLowerCase();
+    const a = (await promptUser(PAD + `${C.cyan}◌${C.reset} ${C.dim}Did you mean${C.reset} ${C.cyan}${suggestion}${C.reset}${C.dim}?${C.reset} [y/N]`, 'no')).toLowerCase();
     if (['y', 'yes'].includes(a)) return true;
     if (['n', 'no'].includes(a)) return false;
     warn('Please answer [Y] Yes or [N] No)');
