f75a01f2c35aa12c462d02142e83bdb69350ef13
IJuanTM <iwan2510@gmail.com>
2026-05-06 23:14:12 +0200

    Reverted removal of default value for user prompt

--- install.js
+++ install.js
@@ -110,7 +110,8 @@ const downloadFile = (url, dest) => new Promise((resolve, reject) => {
 
 const promptUser = (question, defaultValue = '') => new Promise(resolve => {
   const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-  rl.question(`${question}: `, answer => {
+  const prompt = defaultValue ? `${question} ${C.dim}(${defaultValue})${C.reset}: ` : `${question}: `;
+  rl.question(prompt, answer => {
     rl.close();
     resolve(answer.trim() || defaultValue);
   });
@@ -155,7 +156,7 @@ const listAddons = (addons) => addons.forEach((name, i) => out(PAD + C.cyan + `$
 const confirmSuggestion = async (suggestion) => {
   line();
   while (true) {
-    const a = (await promptUser(PAD + `${C.cyan}◌${C.reset} ${C.dim}Did you mean${C.reset} ${C.cyan}${suggestion}${C.reset}${C.dim}?${C.reset} [y/N]`, 'no')).toLowerCase();
+    const a = (await promptUser(PAD + `${C.cyan}◌${C.reset} ${C.dim}Did you mean${C.reset} ${C.cyan}${suggestion}${C.reset}${C.dim}?${C.reset}`, 'n')).toLowerCase();
     if (['y', 'yes'].includes(a)) return true;
     if (['n', 'no'].includes(a)) return false;
     warn('Please answer [Y] Yes or [N] No)');
